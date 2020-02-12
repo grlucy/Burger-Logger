f23f49006fab6999bbaa89963d942bd6f73a742e
unknown <53538316+grlucy@users.noreply.github.com>
2020-02-12 13:44:42 -0500

    wrote schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+-- Create the burgers_db database and burgers table
+
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+ id INTEGER AUTO_INCREMENT NOT NULL,
+ burger_name VARCHAR(100) NULL,
+ devoured BOOLEAN NOT NULL DEFAULT 0,
+ PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert queries to populate the `burgers` table with at least three entries
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cheeseburger"), ("Chili burger"), ("Slider");
