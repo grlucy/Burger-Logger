2917ab3647c16ba28caaf59591ec73dc3e382b86
unknown <53538316+grlucy@users.noreply.github.com>
2020-02-12 13:52:02 -0500

    set up MySQL connection and export

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Enter your MySQL password in the string below:
+
+const password = "";
+
+// MySQL connection
+
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: password,
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection
+
+module.exports = connection;
